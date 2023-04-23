ff318ce54ba261a43e969000d77149c9d04d78fb
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-04-23 16:47:18 -0500

    built out select and options for feeling component

--- src/components/Loop/Feeling.jsx
+++ src/components/Loop/Feeling.jsx
@@ -5,6 +5,14 @@ function Feeling() {
     return (
         <>
             <h1>How are you feeling today?</h1>
+            <label for ="feeling">Select a feeling:</label>
+            <br />
+            <select id="feeling">
+                <option value="great">Great!</option>
+                <option value="reallygood">Really good!</option>
+                <option value="fantastic">Fantastic!</option>
+                <option value="news">I read the news and am insufferably sad!</option>
+            </select>
         </>
     );
 }
