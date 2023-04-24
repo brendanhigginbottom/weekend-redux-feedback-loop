88ad321e662ef90a23df3223b7bf334663ae111e
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-04-24 18:00:41 -0500

    added color selector and added imports for redux and react router

--- src/components/Loop/Support.jsx
+++ src/components/Loop/Support.jsx
@@ -4,7 +4,12 @@ function Support() {
 
     return (
         <>
-            <h1>Support</h1>
+            <h1>How well are you feeling supported today?</h1>
+            <br />
+            <form>
+                <label htmlFor="support">Describe your suport as a color:</label><br />
+                <input type="color" id="support" name="support" />
+            </form>
         </>
     );
 }
