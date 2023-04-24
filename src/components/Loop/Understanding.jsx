8b402d0b17deb364e28070c471cc6a20b2399e5b
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-04-24 17:38:05 -0500

    added radio buttons with selections

--- src/components/Loop/Understanding.jsx
+++ src/components/Loop/Understanding.jsx
@@ -4,7 +4,16 @@ function Understanding() {
 
     return (
         <>
-            <h1>Understanding</h1>
+            <h1>How are you standing the content?</h1>
+            <input type="radio" id="under" value="Under standing" />
+            <label htmlFor="under">Under standing</label>
+            <br />
+            <input type="radio" id="even" value="Evenly standing" />
+            <label htmlFor="even">Evenly standing</label>
+            <br />
+            <input type="radio" id="over" value="Over standing" />
+            <label htmlFor="over">Over standing</label>
+
         </>
     );
 }
