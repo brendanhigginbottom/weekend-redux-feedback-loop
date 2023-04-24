1c3497ca3f54dce68e32420f0f6c5ba9eb6b2d61
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-04-24 17:51:58 -0500

    added validation and pushing to /support

--- src/components/Loop/Understanding.jsx
+++ src/components/Loop/Understanding.jsx
@@ -4,6 +4,7 @@ import { useHistory } from 'react-router-dom';
 function Understanding() {
     const dispatch = useDispatch();
     const understanding = useSelector(store => store.understandingValue);
+    const history = useHistory();
 
     const handleUnderstanding = (event) => {
         const action = {
@@ -13,10 +14,18 @@ function Understanding() {
         dispatch(action);
     }
 
+    const nextPage = () => {
+        if (understanding !== '') {
+            history.push('/support');
+        } else {
+            alert('Please select a value!');
+        }
+    };
+
     return (
         <>
             <h1>How are you standing the content?</h1>
-            <h2>{understanding}</h2>
+            {/* <h2>{understanding}</h2> */}
             <input type="radio" onChange={handleUnderstanding} name="standing" id="under" value="Under standing" />
             <label htmlFor="under">Under standing</label>
             <br />
@@ -25,6 +34,8 @@ function Understanding() {
             <br />
             <input type="radio" onChange={handleUnderstanding} name="standing" id="over" value="Over standing" />
             <label htmlFor="over">Over standing</label>
+            <br />
+            <button onClick={nextPage}>Next</button>
         </>
     );
 }
