61cd49534007c9c95217ec10281a23e1d611cb90
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-04-24 17:43:21 -0500

    imported redux and history, wrote change handler to send to redux

--- src/components/Loop/Understanding.jsx
+++ src/components/Loop/Understanding.jsx
@@ -1,17 +1,26 @@
-
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 function Understanding() {
+
+    const handleUnderstanding = (event) => {
+        const action = {
+            type: 'SET_UNDERSTANDING',
+            payload: event.target.value,
+        }
+        dispatchEvent(action);
+    }
 
     return (
         <>
             <h1>How are you standing the content?</h1>
-            <input type="radio" id="under" value="Under standing" />
+            <input type="radio" onChange={handleUnderstanding} id="under" value="Under standing" />
             <label htmlFor="under">Under standing</label>
             <br />
-            <input type="radio" id="even" value="Evenly standing" />
+            <input type="radio" onChange={handleUnderstanding} id="even" value="Evenly standing" />
             <label htmlFor="even">Evenly standing</label>
             <br />
-            <input type="radio" id="over" value="Over standing" />
+            <input type="radio" onChange={handleUnderstanding} id="over" value="Over standing" />
             <label htmlFor="over">Over standing</label>
         </>
     );
