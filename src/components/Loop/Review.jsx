6c95ccd9eab9692491c959b3bfb19e3166c7c457
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-04-24 18:52:22 -0500

    added redux selectors and baseline layout

--- src/components/Loop/Review.jsx
+++ src/components/Loop/Review.jsx
@@ -1,10 +1,22 @@
-
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 function Review() {
+    const feeling = useSelector(store => store.feelingValue);
+    const understanding = useSelector(store => store.understandingValue);
+    const support = useSelector(store => store.supportValue);
+    const comments = useSelector(store => store.commentsValue);
 
     return (
         <>
             <h1>Review</h1>
+            <br />
+            <div>
+                <p>Feeling: {feeling}</p>
+                <p>Standing: {understanding}</p>
+                <p>Support: {support}</p>
+                <p>Comments: {comments}</p>
+            </div>
         </>
     );
 }
